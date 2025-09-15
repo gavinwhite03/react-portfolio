ac2c5b65cffe529a5a8b3510d5c0af5bd8634fdc
gavinwhite03 <anothertexan03@hotmail.com>
2025-09-15 23:56:40 +0200

    Restructered App.js for recipe layout

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Contact from "./components/Contact";
 import RecipeHome from './components/recipe-app/Home.tsx';
 import Recipes from './components/recipe-app/Recipes.tsx';
 import RecipeDetail from './components/recipe-app/RecipeDetails.tsx';
-import Pantry from './components/recipe-app/Pantry.tsx'
-import RecipeBanner from './components/recipe-app/recipeComponents/Banner.tsx'
-import RecipeFooter from './components/recipe-app/recipeComponents/Footer.tsx'
-import RecipeNavbar from './components/recipe-app/recipeComponents/Navbar.tsx'
+import Pantry from './components/recipe-app/Pantry.tsx';
+import RecipeBanner from './components/recipe-app/recipeComponents/Banner.tsx';
+import RecipeFooter from './components/recipe-app/recipeComponents/Footer.tsx';
+import RecipeNavbar from './components/recipe-app/recipeComponents/Navbar.tsx';
 
-function App() {
+// Main portfolio layout component
+function PortfolioLayout() {
   return (
     <div>
       <Navbar />
@@ -22,11 +23,52 @@ function App() {
       <Skills />
       <Work />
       <Contact />
-      <Route path="/whats-4-dinner" element={<RecipeHome />} />
-      <Route path="/whats-4-dinner/recipes" element={<Recipes />} />
-      <Route path="/whats-4-dinner/recipes/:id" element={<RecipeDetail />} />
-      <Route path="/whats-4-dinner/pantry" element={<Pantry />} />
     </div>
+  );
+}
+
+// Recipe app layout component with recipe-specific navbar/footer
+function RecipeLayout({ children }) {
+  return (
+    <div>
+      <RecipeBanner />
+      <RecipeNavbar />
+      {children}
+      <RecipeFooter />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Main portfolio page */}
+        <Route path="/" element={<PortfolioLayout />} />
+        
+        {/* Recipe app routes with recipe layout */}
+        <Route path="/whats-4-dinner" element={
+          <RecipeLayout>
+            <RecipeHome />
+          </RecipeLayout>
+        } />
+        <Route path="/whats-4-dinner/recipes" element={
+          <RecipeLayout>
+            <Recipes />
+          </RecipeLayout>
+        } />
+        <Route path="/whats-4-dinner/recipes/:id" element={
+          <RecipeLayout>
+            <RecipeDetail />
+          </RecipeLayout>
+        } />
+        <Route path="/whats-4-dinner/pantry" element={
+          <RecipeLayout>
+            <Pantry />
+          </RecipeLayout>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
