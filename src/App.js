cb349a73946d849016dfcf2eff6eaa9699d286d4
gavinwhite03 <anothertexan03@hotmail.com>
2025-09-15 23:52:41 +0200

    Fixing recipe imports

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Skills from "./components/Skills";
 import Home from "./components/home";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-import RecipeHome from './components/recipe-app/Home';
-import Recipes from './components/recipe-app/Recipes';
-import RecipeDetail from './components/recipe-app/RecipeDetail';
-import Pantry from './components/recipe-app/Pantry'
-import RecipeBanner from 'src/components/recipe-app/recipeComponents/Banner.tsx'
-import RecipeFooter from 'src/components/recipe-app/recipeComponents/Footer.tsx'
-import RecipeNavbar from 'src/components/recipe-app/recipeComponents/Navbar.tsx'
+import RecipeHome from './components/recipe-app/Home.tsx';
+import Recipes from './components/recipe-app/Recipes.tsx';
+import RecipeDetail from './components/recipe-app/RecipeDetails.tsx';
+import Pantry from './components/recipe-app/Pantry.tsx'
+import RecipeBanner from './components/recipe-app/recipeComponents/Banner.tsx'
+import RecipeFooter from './components/recipe-app/recipeComponents/Footer.tsx'
+import RecipeNavbar from './components/recipe-app/recipeComponents/Navbar.tsx'
 
 function App() {
   return (
