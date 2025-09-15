78ce085864840ca6a2f31700515fa048dbc306a6
gavinwhite03 <anothertexan03@hotmail.com>
2025-09-16 00:13:48 +0200

    Fixing More Imports

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import premScraping from '../assests/premScraping.png'
 import personalFinances from '../assests/personalFinances.png'
 import LMS from '../assests/LMS.png'
